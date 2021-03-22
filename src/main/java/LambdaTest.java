d54ed61a2c44baf6c6e702db0fdf81e473da4780
sudheer.kumar <sudheer.kumar@IRIS-CSG-3042.irissoftware.com>
2021-03-22 14:04:26 +0530

    added some functional inetrfaces methods

--- src/main/java/LambdaTest.java
+++ src/main/java/LambdaTest.java
@@ -1,21 +1,41 @@
 
-public class LambdaTest {
+public class LambdaTest// implements FunctionalTest 
+{
+	/*@Override
+	public void m1() {
+		System.out.println(" approach beforte java 8 ");
+
+	}*/
 
 	public static void main(String[] args) {
-		//new AnonymusFn().m1();
-		FunctionalTest ft=	()->{
+		// new AnonymusFn().m1();
+		//new LambdaTest().m1();
+		/*FunctionalTest ft = () -> {
 			System.out.print(" func interface m1 mthod ");
+			
 		};
-		ft.m1();
-		ft.m3();
-		FunctionalTest.m4();
+		
+		FunctionalTest ft = () -> {
+			System.out.print(" func interface m1 mthod ");
+			return 0;
+		};*/
+		FunctionalTest ft = (n1,n2) -> {
+			System.out.print(" func interface m1 mthod ");
+			return n1+n2;
+		};
+		int m1 = ft.sum(2,3);
+		System.out.println("return type functional method value:: "+m1);
+		//ft.m3();
+		//FunctionalTest.m4();
 	}
 
 }
 
 @FunctionalInterface
 interface FunctionalTest {
-	void m1();
+	//void m1();
+	//int m1();
+	int sum(int n1,int n2);
 	// void m2(); can't be add second method as it's functional interface
 
 	default void m2() {
